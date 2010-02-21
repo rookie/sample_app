171946ea87a520437ec5bda091f2081c6e4d0c95
Matt <mhurley@Matts-MacBook-Pro.local>
2010-02-21 14:07:37 -0800

    Add stylesheets and helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  # Return a title on a per-page basis.               # Documentation comment
+  def title                                           # Method definition
+    base_title = "Ruby on Rails Tutorial Sample App"  # Variable assignment
+    if @title.nil?                                    # Boolean test for nil
+      base_title                                      # Implicit return
+    else
+      "#{base_title} | #{@title}"                     # String interpolation
+    end
+  end
+
 end
